1df21e553a1f9840e9b20b06372111a650387013
dvicjon <devinvjones@gmail.com>
2021-04-23 09:25:13 -0400

    project setup

---
+++ src/components/Project/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Modal from '../Modal';
+
+const Project = ({ category }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentPhoto] = useState();
+
+    const [photos] = useState([
+        {
+            name: '',
+            category: '',
+            description: ''
+        },
+
+    ])
+    
+    const currentPhotos = photos.filter((photo) => photo.category === category);
+
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (
+        <div>
+          {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
+          <div className="flex-row">
+            {currentPhotos.map((image, i) => (
+              <img
+                src={require(`../../assets/small/${category}/${i}.jpg`).default}
+                alt={image.name}
+                className=""
+                onClick={() => toggleModal(image, i)}
+                key={image.name}
+              />
+            ))}
+          </div>
+        </div>
+    );
+};
+
+export default Project; 
