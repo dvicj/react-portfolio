5c02792230aff7b03b9c040e4e4285b1fcfc5f58
dvicjon <devinvjones@gmail.com>
2021-04-23 20:25:05 -0400

    added icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faSpotify} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
 
   return (
-    <section>
-      
+    <section className="foot-sec">
+      <FontAwesomeIcon icon={faGithub}/>
+      <FontAwesomeIcon icon={faInstagram}/>
+      <FontAwesomeIcon icon={faLinkedin}/>
+      <FontAwesomeIcon icon={faSpotify}/>
+      <FontAwesomeIcon icon={faEnvelope}/>
     </section>
   )
 }
