d4be1553acd926b8491eef8895efc6efc145c6bf
dvicjon <devinvjones@gmail.com>
2021-04-24 22:01:00 -0400

    photos

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import aboutImage from '../../assets/cover/about-me-photo.jpg'
+import resumeImage from '../../assets/cover/Devin-Jones-Resume.png'
 
 function About() {
 
@@ -6,12 +8,13 @@ function About() {
     <section className="my-5 about-sec">
       <h1 id="about">ABOUT ME</h1>
       <div className="my-2">
+        <img id="about-image" src={aboutImage} alt='me'/>
         <p>
           Thanks for checking out my portfolio! My name is Devin. I am a 27 year old living in Ontario, Canada. 
           I am learning to code and I love creating beautiful and functional websites from scratch. Feel free to check me
           out at any of the links below. Thanks! 
       </p>
-      <a className="resume-dl" href="resume.html" title="Devin Jones Resume" download>Download my resume</a>
+      <a className="resume-dl" href={resumeImage} title="Devin Jones Resume" download>Download my resume</a>
       </div>
     </section>
   )
