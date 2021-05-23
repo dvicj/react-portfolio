8656b1db560708837cb166bb711393d63ab8a9fb
dvicjon <devinvjones@gmail.com>
2021-05-23 10:14:39 -0400

    about me style changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,14 +6,14 @@ function About() {
 
   return (
     <section className="my-5 about-sec">
-      <h1 id="about">ABOUT ME</h1>
       <div className="my-2">
+      <h1 id="about">ABOUT ME</h1>
         <img id="about-image" src={aboutImage} alt='me'/>
-        <p>
+        <p id="about-text">
           Thanks for checking out my portfolio! My name is Devin. I am a 27 year old living in Ontario, Canada. 
           I am learning to code and I love creating beautiful and functional websites from scratch. Feel free to check me
           out at any of the links below. Thanks! 
-      </p>
+        </p>
       <a className="resume-dl" href={resumeImage} title="Devin Jones Resume" download>Download my resume</a>
       </div>
     </section>
